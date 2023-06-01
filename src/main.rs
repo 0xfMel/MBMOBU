2ebd584365b8387c6bf8a1fead91cfffb21068bc
0xfMel <97613995+0xfMel@users.noreply.github.com>
2023-06-01 14:49:26 +0100

    bulk compression & file scheduling

--- src/main.rs
+++ src/main.rs
@@ -97,18 +97,34 @@
 // Can't be easily fixed
 #![allow(clippy::multiple_crate_versions)]
 
-use std::path::Path;
+use std::{
+    cell::Cell,
+    io::{self, ErrorKind, Write},
+    marker::PhantomData,
+    path::{Path, PathBuf},
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc, Weak,
+    },
+};
 
 use anyhow::Context;
 use memmap2::Mmap;
+use parking_lot::Mutex;
 use tokio::{
     fs::File,
+    sync::Notify,
     task::{self, JoinSet},
 };
 use walkdir::WalkDir;
+use zstd_safe::zstd_sys::ZSTD_EndDirective;
 
 const COMPRESS_TASKS: usize = 6;
+const LEVEL: i32 = 19;
 const BLOCK: u64 = (25 * 1024 * 1024) - 512;
+#[allow(clippy::cast_possible_truncation)]
+const BLOCK_USIZE: usize = BLOCK as usize;
+const DEFAULT_BUF_SIZE: usize = 8 * 1024;
 
 #[tokio::main]
 async fn main() {
@@ -116,35 +132,47 @@ async fn main() {
     let (walk_tx, walk_rx) = kanal::bounded(1);
 
     let walker = task::spawn_blocking(move || {
+        let mut id = 0;
         for entry in WalkDir::new(base_path) {
             let entry = entry.context("Error walking")?;
             if !entry.file_type().is_file() {
                 continue;
             }
 
-            if walk_tx.send(entry.into_path()).is_err() {
+            if walk_tx
+                .send(FileEntry {
+                    id,
+                    path: entry.into_path(),
+                })
+                .is_err()
+            {
                 break;
             }
+            id += 1;
         }
 
         Ok::<_, anyhow::Error>(())
     });
 
     let mut compress_set = JoinSet::new();
+    let file_scheduler = FileScheduler::new();
     for _ in 0..COMPRESS_TASKS {
+        let mut file_scheduler_handle = file_scheduler.new_handle();
         let base_path = base_path.to_path_buf();
         let walk_rx = walk_rx.clone_async();
         compress_set.spawn(async move {
+            let mut bulk = BulkCompressor::new().context("Failed to create bulk compressor")?;
+
             loop {
-                let Ok(asbsolute) = walk_rx.recv().await else {
+                let Ok(FileEntry { id, path: absolute }) = walk_rx.recv().await else {
                     break;
                 };
 
-                let file = File::open(&asbsolute)
+                let file = File::open(&absolute)
                     .await
-                    .with_context(|| format!("Failed to open file: {}", asbsolute.display()))?;
+                    .with_context(|| format!("Failed to open file: {}", absolute.display()))?;
                 let meta = file.metadata().await.with_context(|| {
-                    format!("Failed to fetch metadata for file: {}", asbsolute.display())
+                    format!("Failed to fetch metadata for file: {}", absolute.display())
                 })?;
                 // SAFETY: TODO
                 let mmap = unsafe { Mmap::map(&file) }.context("Failed to mmap file")?;
@@ -153,22 +181,94 @@ async fn main() {
                 let mut header = tar::Header::new_gnu();
                 header.set_metadata(&meta);
 
-                let relative = asbsolute.strip_prefix(&base_path).with_context(|| {
-                    format!("Failed to strip prefix for path: {}", asbsolute.display())
+                let relative = absolute.strip_prefix(&base_path).with_context(|| {
+                    format!("Failed to strip prefix for path: {}", absolute.display())
                 })?;
                 let max_compressed_size = max_compressed_size(meta.len(), relative)?;
 
                 if max_compressed_size > BLOCK {
+                    file_scheduler_handle.wait_for(id).await;
                     todo!("stream compression");
                 } else {
-                    todo!("bulk compression");
+                    let bulk_buf = bulk.compress(&mmap, header, relative).with_context(|| {
+                        format!("Failed to bulk compress {}", absolute.display())
+                    })?;
+
+                    file_scheduler_handle.wait_for(id).await;
                 }
+
+                file_scheduler_handle.next();
             }
 
             Ok::<_, anyhow::Error>(())
         });
     }
     drop(walk_rx);
+}
+
+struct FileEntry {
+    id: usize,
+    path: PathBuf,
+}
+
+struct FileSchedulerInner {
+    notifiers: Mutex<Vec<Weak<Notify>>>,
+    file_id: AtomicUsize,
+}
+
+struct FileScheduler {
+    inner: Arc<FileSchedulerInner>,
+}
+
+struct FileSchedulerHandle {
+    inner: Arc<FileSchedulerInner>,
+    notify: Arc<Notify>,
+}
+
+impl FileScheduler {
+    fn new() -> Self {
+        Self {
+            inner: Arc::new(FileSchedulerInner {
+                notifiers: Mutex::new(Vec::new()),
+                file_id: AtomicUsize::new(0),
+            }),
+        }
+    }
+
+    fn new_handle(&self) -> FileSchedulerHandle {
+        let notify = Arc::new(Notify::new());
+        self.inner.notifiers.lock().push(Arc::downgrade(&notify));
+        FileSchedulerHandle {
+            inner: Arc::clone(&self.inner),
+            notify,
+        }
+    }
+}
+
+impl FileSchedulerHandle {
+    async fn wait_for(&mut self, id: usize) {
+        loop {
+            if self.inner.file_id.load(Ordering::Acquire) == id {
+                break;
+            }
+
+            self.notify.notified().await;
+        }
+    }
+
+    fn next(&self) {
+        self.inner.file_id.fetch_add(1, Ordering::Release);
+        self.notify_all();
+    }
+
+    fn notify_all(&self) {
+        self.inner.notifiers.lock().retain(|n| {
+            n.upgrade().map_or(false, |n| {
+                n.notify_one();
+                true
+            })
+        });
+    }
 }
 
 const TAR_BLOCK: u64 = 512;
@@ -201,4 +301,182 @@ pub fn usize_to_u64(value: usize) -> u64 {
     value
         .try_into()
         .expect("usize value doesn't fit inside u64")
+}
+
+struct BulkCompressor<'a> {
+    tar_zstd: TarZstd<'a, Vec<u8>>,
+}
+
+impl BulkCompressor<'_> {
+    fn new() -> anyhow::Result<Self> {
+        let mut tar_zstd = TarZstd::new(Vec::with_capacity(BLOCK_USIZE));
+        tar_zstd
+            .set_level(LEVEL)
+            .context("Failed to set zstd compression level")?;
+        tar_zstd
+            .set_long()
+            .context("Failed to set zstd long range matching")?;
+        Ok(Self { tar_zstd })
+    }
+
+    fn compress<P: AsRef<Path>>(
+        &mut self,
+        mmap: &Mmap,
+        mut header: tar::Header,
+        relative: P,
+    ) -> anyhow::Result<&mut Vec<u8>> {
+        task::block_in_place(|| {
+            self.tar_zstd
+                .append_file(&mut header, relative, mmap)
+                .context("Failed to append file to archive")?;
+            self.tar_zstd
+                .end_frame()
+                .context("Failed to end zstd frame")?;
+
+            Ok::<_, anyhow::Error>(self.tar_zstd.inner_mut())
+        })
+    }
+}
+
+struct TarZstd<'a, W: Write> {
+    inner: tar::Builder<Zstd<'a, W>>,
+}
+
+impl<W: Write> TarZstd<'_, W> {
+    pub fn new(inner: W) -> Self {
+        Self {
+            inner: tar::Builder::new(Zstd::new(inner)),
+        }
+    }
+
+    pub fn set_level(&mut self, level: i32) -> io::Result<()> {
+        self.inner.get_mut().set_level(level)
+    }
+
+    pub fn set_long(&mut self) -> io::Result<()> {
+        self.inner.get_mut().set_long()
+    }
+
+    pub fn end_frame(&mut self) -> io::Result<()>
+    where
+        W: Write,
+    {
+        self.inner.get_mut().end_frame()
+    }
+
+    pub fn inner_mut(&mut self) -> &mut W {
+        self.inner.get_mut().inner_mut()
+    }
+
+    pub fn append_file<P: AsRef<Path>>(
+        &mut self,
+        header: &mut tar::Header,
+        path: P,
+        data: &[u8],
+    ) -> io::Result<()> {
+        self.inner.append_data(header, path, data)
+    }
+}
+
+struct Zstd<'a, W> {
+    cctx: zstd_safe::CCtx<'a>,
+    inner: W,
+    buf: Vec<u8>,
+}
+
+impl<W> Zstd<'_, W> {
+    pub fn new(inner: W) -> Self {
+        Self {
+            cctx: zstd_safe::CCtx::create(),
+            inner,
+            buf: Vec::with_capacity(DEFAULT_BUF_SIZE),
+        }
+    }
+
+    pub fn set_level(&mut self, level: i32) -> io::Result<()> {
+        self.cctx
+            .set_parameter(zstd_safe::CParameter::CompressionLevel(level))
+            .map_err(map_zstd_error)?;
+        Ok(())
+    }
+
+    pub fn set_long(&mut self) -> io::Result<()> {
+        self.cctx
+            .set_parameter(zstd_safe::CParameter::EnableLongDistanceMatching(true))
+            .map_err(map_zstd_error)?;
+        Ok(())
+    }
+
+    pub fn end_frame(&mut self) -> io::Result<()>
+    where
+        W: Write,
+    {
+        loop {
+            if self.flush_with(ZSTD_EndDirective::ZSTD_e_end)? == 0 {
+                break;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn flush_with(&mut self, directive: ZSTD_EndDirective) -> io::Result<usize>
+    where
+        W: Write,
+    {
+        let mut in_buf = zstd_safe::InBuffer::around(&[]);
+        let mut out_buf = zstd_safe::OutBuffer::around(&mut self.buf);
+        let remaining = self
+            .cctx
+            .compress_stream2(&mut out_buf, &mut in_buf, directive)
+            .map_err(map_zstd_error)?;
+
+        self.inner.write_all(&self.buf)?;
+        self.buf.clear();
+
+        self.inner.flush()?;
+        Ok(remaining)
+    }
+
+    pub fn inner_mut(&mut self) -> &mut W {
+        &mut self.inner
+    }
+}
+
+impl<W: Write> Write for Zstd<'_, W> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        loop {
+            let mut in_buf = zstd_safe::InBuffer::around(buf);
+            let mut out_buf = zstd_safe::OutBuffer::around(&mut self.buf);
+            self.cctx
+                .compress_stream2(
+                    &mut out_buf,
+                    &mut in_buf,
+                    ZSTD_EndDirective::ZSTD_e_continue,
+                )
+                .map_err(map_zstd_error)?;
+            self.inner.write_all(&self.buf)?;
+            self.buf.clear();
+
+            let written = in_buf.pos();
+            if written > 0 || buf.is_empty() {
+                return Ok(written);
+            }
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        loop {
+            if self.flush_with(ZSTD_EndDirective::ZSTD_e_flush)? == 0 {
+                break;
+            }
+        }
+
+        self.inner.flush()
+    }
+}
+
+fn map_zstd_error(code: usize) -> io::Error {
+    let msg = zstd_safe::get_error_name(code);
+    io::Error::new(ErrorKind::Other, msg.to_owned())
 }
